7aba5ec98539a14c285a594575db593ceb78850f
tmxiong <990895247@qq.com>
2018-02-06 19:53:31 +0800

    -

---
+++ pages/index/index.js
@@ -0,0 +1,83 @@
+// pages/index.js
+import urls from '../../utils/urls.js'
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    secret: 'eb7f104bd5c44f5fb6862b3b9a4b31af',
+    url: '',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    wx.request({
+      url: urls.getLotteryKind(),
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function (res) {
+        // success
+        that.setData({
+          list: res.data.data
+        })
+
+        console.log(res.data)
+
+      },
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ utils/urls.js
@@ -0,0 +1,34 @@
+// let urls = {
+
+// };
+// module.exports = urls;
+
+let timeStamp = function () {
+  return new Date().getTime();
+};
+
+// 福彩3d/高频彩/数字彩的列表链接
+exports.getPlayTips = function (type, offset) {
+  // type = fc/gpc/szc/csxw
+  return 'https://m.qmcai.com/support/cmsv2/information/queryContent?parameter=%7B%22command%22:%22queryContent%22,%22categoryId%22:%22' + type + '%22,%22offset%22:' + offset + ',%22size%22:15,%22platform%22:%22html%22,%22version%22:%225.2.16%22%7D&callback=jsonp5'
+};
+
+// 福彩3d/高频彩/数字彩的详情链接
+exports.getPlayTipsDetail = function (id) {
+  return 'https://m.qmcai.com/zixun/detail.html?_id=' + id + '&time=' + timeStamp();
+};
+
+// 彩票彩种列表
+exports.getLotteryKind = function () {
+  return 'https://api.niubcaipiao.com/Prizes/getList'
+}
+
+// 彩票开奖列表
+exports.getLotteryList = function() {
+  return 'https://api.niubcaipiao.com/TwoColorBall/getPrizes'
+}
+
+// 彩票销售数据
+exports.getLotteryPrice = function () {
+  return 'https://api.niubcaipiao.com/TwoColorBall/getPrize?issue=2018015'
+}
